1ee92e13604ff7b426c3336a1afde6c3798e67e2
llwang8 <wangxbl56@gmail.com>
2015-11-27 08:41:09 -0500

    fix route path error

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -4,7 +4,8 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
 
   test "layout links" do
     get root_path
-    puts response.body
+    #puts response.body
+    puts assert_select 'a[href]'
     assert_template 'static_pages/home'
     assert_select "a[href=?]", root_path
     assert_select "a[href=?]", help_path
