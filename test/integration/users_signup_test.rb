3b96b0d66edde8551c393c72a006625981e04395
llwang8 <wangxbl56@gmail.com>
2015-11-30 16:14:20 -0500

    Add password resets & email configuration - almost done

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -20,18 +20,19 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
   test "valid signup information with account activation" do
     get signup_path
     assert_difference 'User.count', 1 do
-      post users_path, user: {
-          name:  "Example User",
-          email: "user@example.com",
-          password:              "password",
-          password_confirmation: "password" }
+      post users_path, user: { name:  "Example User",
+                               email: "user@example.com",
+                               password:              "password",
+                               password_confirmation: "password" }
     end
 
     assert_equal 1, ActionMailer::Base.deliveries.size
     user = assigns(:user)
-    assert_not user.activated?
-    log_in_as(user)
+    assert user.activated?
+
     assert_not is_logged_in?
+    log_in_as(user)
+    assert is_logged_in?
     get edit_account_activation_path("invalid token")
     assert_not is_logged_in?
     get edit_account_activation_path(user.activation_token, email: 'wrong')
